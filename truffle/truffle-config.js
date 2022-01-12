ef48b140f9581557700a63e135592b0a302dc241
AlphanetRA <alphanetra0321@gmail.com>
2022-01-12 14:38:54 +0900

    Add public_key field to .env.example

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -34,9 +34,9 @@ module.exports = {
 
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
       from: process.env.PUBLIC_KEY
     },
     cronos: {
